4f9a5465cbeefe9fd11011a77c847fb4ebd75175
duy380 <64094479+duy380@users.noreply.github.com>
2021-11-10 19:57:06 +0700

    Update script

--- PA1_script.sql
+++ PA1_script.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ef2b7170f4f5e0c73aa31c15bd776b0b6e3ebb3c6d5f74e94fd9eb730784e9c2
-size 5447
+oid sha256:bc5e7e3454fcfd7b7c3c7b697d3f5bb16ffc51a03e6633b0a657d34252319f1d
+size 5739
