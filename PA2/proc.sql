1c8daa6d95bdc16fd5e50d80b65580172c86e222
Nguyễn Quang Trường <kuutruong@gmail.com>
2021-12-30 21:35:44 +0700

    Create proc.sql

---
+++ PA2/proc.sql
@@ -0,0 +1,26 @@
+﻿USE Hoayeuthuong
+go
+
+create or alter proc checkTK
+@username varchar(30),
+@password char(30)
+as
+begin
+        if not exists(select TK_ID from TAIKHOAN where TK_USERNAME=@username)
+            begin
+                raiserror(N'Sai tên đăng nhập!',0,1);
+                return
+            end
+        if not exists(select TK_ID from TAIKHOAN where TK_USERNAME=@username and TK_PASSWORD=@password)
+            begin
+                raiserror('Sai mật khẩu!',0,1);
+                return
+            end
+        if exists(select TK_ID from TAIKHOAN where TK_USERNAME=@username and TK_PASSWORD=@password)
+            begin 
+                select TK_ID, TK_ROLE from TAIKHOAN where TK_USERNAME=@username and TK_PASSWORD=@password
+            end
+end
+
+--select * from TAIKHOAN
+--exec checkTK 'Matthew459', 'GK7NWPFRM'
